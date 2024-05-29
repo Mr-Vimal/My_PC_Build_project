c05a868812f18b2077208d8930ae5660a2581339
Mr-Vimal <mrvimal0501@gmail.com>
2024-05-29 14:24:40 +0530

    vercel2.2

--- Client/src/Pages/Home/Home.js
+++ Client/src/Pages/Home/Home.js
@@ -21,7 +21,7 @@ export default function Home() {
                 </div>
             </div>
             <Products />
-            <Payment />
+            {/* <Payment /> */}
             <Footer />
         </>
     );
